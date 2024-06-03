034cc36db7dab387b66bce98b267b7a75c13fe64
Fernando Dias Motta <fernando3dias@gmail.com>
2024-06-02 23:01:46 -0300

    Iniciando o blazor

--- Fina.Common/Configuration.cs
+++ Fina.Common/Configuration.cs
@@ -6,7 +6,7 @@
         public const int DefaultPageNumber = 1;
         public const int DetaultPageSize = 25;
 
-        public static string BackendUrl { get; set; } = string.Empty;
-        public static string FrontendUrl { get; set; } = string.Empty;
+        public static string BackendUrl { get; set; } = "http://localhost:5165";
+        public static string FrontendUrl { get; set; } = "http://localhost:5100";
     }
 }
